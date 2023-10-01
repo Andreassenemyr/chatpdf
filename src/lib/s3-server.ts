ce2245902e3f6af86472e12497ce567cb2ef9a2e
BuildTools <unconfigured@null.spigotmc.org>
2023-10-01 21:28:46 +0200

    AWS S3

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -4,7 +4,7 @@ export async function downloadFromS3(file_key: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
       const s3 = new S3({
-        region: process.env.NEXT_PUBLIC_S3_REGION!,
+        region: 'eu-north-1',
         credentials: {
           accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
           secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!,
